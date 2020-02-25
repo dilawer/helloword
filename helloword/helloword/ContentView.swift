6a9713eaf37a8c1b64bba02b753fe51ba447a0fb
dilawer <info.dilawer@gmail.com>
2020-02-25 12:50:12 +0500

    test coment

---
+++ helloword/helloword/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  helloword
+//
+//  Created by Si on 2/25/20.
+//  Copyright © 2020 Si. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        //testtttt
+    }
+}
